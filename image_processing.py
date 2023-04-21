1a5a367120464ec408f2d96c194dfe005ce4d375
uupb <93621948+uupb@users.noreply.github.com>
2023-04-21 15:54:17 -0700

    Summary of Changes
    '''
    main.py
    
    Replaced the calls to process_image() with calls to process_image(input_path, MIN_DISTANCE) in main.py.
    Added a constant variable for the minimum distance at the top of main.py:
    MIN_DISTANCE = 20 # You can change this value as needed
    
    image_processing.py
    
    Modified the process_image() function to accept an additional argument min_distance and updated the function to use min_distance while filtering contours:
    def process_image(image_path, min_distance):
    ...
    filtered_contours = filter_contours(contours, min_distance)
    ...
    
    Updated the filter_contours() function to accept the min_distance argument and added a new helper function is_min_distance_met() to check if the distance between contours is greater than or equal to min_distance:
    def filter_contours(contours, min_distance):
    ...
    if is_large_area and is_reasonable_aspect_ratio and is_min_distance_met(c, filtered_contours, min_distance):
    filtered_contours.append(c)
    ...
    
    def is_min_distance_met(contour, contours, min_distance):
    ...
    
    Updated the image conversion in process_image() to handle transparent backgrounds correctly:
    if cropped_pil_image.mode != 'RGBA':
    cropped_pil_image = cropped_pil_image.convert('RGBA')
    
    alpha = PILImage.new('L', cropped_pil_image.size, 255)
    cropped_pil_image.putalpha(alpha)
    
    These changes should help improve the cropping of letters with different art inside them and handle the transparent backgrounds. The min_distance parameter allows you to fine-tune the results by filtering contours based on their distance from each other. This way, the program should have fewer false positives when detecting letters.
    
    '''

--- image_processing.py
+++ image_processing.py
@@ -3,9 +3,10 @@ import cv2
 import numpy as np
 from PIL import Image as PILImage
 
-def process_image(image_path):
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def process_image(image_path, min_distance):
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    masked_image = apply_mask(image, min_distance)
+    gray = cv2.cvtColor(masked_image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -19,16 +20,29 @@ def process_image(image_path):
     for i, c in enumerate(filtered_contours):
         x, y, w, h = cv2.boundingRect(c)
         cropped_image = image[y:y + h, x:x + w]
-        cropped_pil_image = PILImage.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
-
-        background = PILImage.new("RGBA", cropped_pil_image.size, (255, 255, 255))
-        cropped_pil_image = cropped_pil_image.convert("RGBA")
-        background.paste(cropped_pil_image, (0, 0), cropped_pil_image)
-        cropped_pil_image = background.convert("RGB")
+        cropped_pil_image = PILImage.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGBA))
 
         cropped_images.append((cropped_pil_image, f"{first_term}_{i + 1}.png"))
 
     return cropped_images
+
+def apply_mask(image, min_distance):
+    # Extract the alpha channel from the image
+    alpha_channel = image[:, :, 3]
+
+    # Create a binary mask using the alpha channel
+    _, mask = cv2.threshold(alpha_channel, 0, 255, cv2.THRESH_BINARY)
+
+    # Apply morphological operations (dilation followed by erosion) to improve the mask
+    kernel_size = int(min_distance / 2)
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    mask = cv2.dilate(mask, kernel, iterations=1)
+    mask = cv2.erode(mask, kernel, iterations=1)
+
+    # Apply the mask to the image so that only the shapes of interest are visible
+    masked_image = cv2.bitwise_and(image, image, mask=mask)
+
+    return masked_image
 
 def filter_contours(contours):
     filtered_contours = []
